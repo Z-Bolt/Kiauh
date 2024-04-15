70f4ea939421c3358155a0119d571abc0747be2a
Vlad <427departament@gmail.com>
2024-04-15 20:57:00 +0300

    Изменена ссылка Fluidd с репозитория fluidd-core на Z-Bolt

--- scripts/fluidd.sh
+++ scripts/fluidd.sh
@@ -319,11 +319,11 @@ function get_fluidd_download_url() {
 
   ### latest download url including pre-releases (alpha, beta, rc)
   latest_tag=$(echo "${tags}" | head -1)
-  latest_url="https://github.com/fluidd-core/fluidd/releases/download/${latest_tag}/fluidd.zip"
+  latest_url="https://github.com/Z-Bolt/fluidd/releases/download/${latest_tag}/fluidd.zip"
 
   ### get stable fluidd download url
   stable_tag=$(echo "${tags}" | grep -E "^v([0-9]+\.?){3}$" | head -1)
-  stable_url="https://github.com/fluidd-core/fluidd/releases/download/${stable_tag}/fluidd.zip"
+  stable_url="https://github.com/Z-Bolt/fluidd/releases/download/${stable_tag}/fluidd.zip"
 
   read_kiauh_ini "${FUNCNAME[0]}"
   if [[ ${fluidd_install_unstable} == "true" ]]; then
@@ -411,7 +411,7 @@ function patch_fluidd_update_manager() {
 [update_manager fluidd]
 type: web
 channel: stable
-repo: fluidd-core/fluidd
+repo: Z-Bolt/fluidd
 path: ~/fluidd
 MOONRAKER_CONF
 
